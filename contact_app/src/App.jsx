14e704c0db8dc2ef1ccdc813d399fe45f7a98c2a
Roshan Arikar <85448130+roshanarikar@users.noreply.github.com>
2022-10-02 15:58:22 +0530

    update contact app

--- contact_app/src/App.jsx
+++ contact_app/src/App.jsx
@@ -1,26 +1,16 @@
+import { useState } from "react"
 import { ContactAdd } from "./components/ContactAdd"
 import { ContactList } from "./components/ContactList"
 import { Header } from "./components/Header"
 
 
 function App() {
-  const contact = [
-    {
-      id: 1,
-      name : "Roshan",
-      email: "roshan@gmail.com"
-    },
-    {
-      id: 2,
-      name : "Akshay",
-      email: "akshay@gmail.com"
-    }
-  ]
+  const [ contacts, setContact] = useState([])
   return (
     <div>
      <Header/>
      <ContactAdd/>
-     <ContactList contact={contact}/>
+     <ContactList contact={contacts}/>
     </div>
   )
 }
--- contact_app/src/components/ContactAdd.jsx
+++ contact_app/src/components/ContactAdd.jsx
@@ -1,21 +1,38 @@
+import React from "react"
 
+class ContactAdd extends React.Component {
+    state = {
+        name: "",
+        email: "",
+    }
 
-export const ContactAdd = () =>{
-    return(
-        <div>
-            <form>
+    add = (e) =>{
+        e.preventDefault()
+        if(this.state.name === "" && this.state.email===""){
+            alert("All fields are mandatory")
+            return
+        }
+    }
+    render(){
+        return(
+            <div>
+            <form onSubmit={this.add}>
             <div>
                 <label>Name</label>
-                <input type="text" />
+                <input type="text" placeholder="Name" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})}/>
             </div>
             <div>
                 <label>Email</label>
-                <input type="email" />
+                <input type="email" placeholder="Email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/>
             </div>
             <div>
                 <button>Add</button>
             </div>
             </form>
         </div>
-    )
+        )
+    }
 }
+
+
+export default ContactAdd;
