0e120618fe4ebf9c78ff855074ba8ca7bad32faa
Roshan Arikar <85448130+roshanarikar@users.noreply.github.com>
2022-10-27 10:32:14 +0530

    counter app done

--- counter_app/src/App.jsx
+++ counter_app/src/App.jsx
@@ -7,26 +7,12 @@ function App() {
 
   return (
     <div className="App">
+      <h3>Counter App</h3>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h3>{count}</h3>
+        <button onClick={() => setCount(count+1)}>Increase</button>
+        <button onClick={()=> setCount(count-1)}>Decrease</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
