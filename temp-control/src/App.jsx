31f071f82d61cd4327ce30cc25d4f75144f00493
Roshan Arikar <85448130+roshanarikar@users.noreply.github.com>
2022-10-28 17:23:40 +0530

    add temp counter

--- temp-control/src/App.jsx
+++ temp-control/src/App.jsx
@@ -3,30 +3,38 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ tempCount, setTempCount] = useState(10)
+  const [tempColor, setTempColor] =  useState("cold")
+
+  const increseTemp = () => {
+    setTempCount(tempCount+1)
+
+    if(tempCount>15){
+      setTempColor("hot")
+    }
+  }
+
+  const decreseTemp = () => {
+    setTempCount(tempCount-1)
+
+    if(tempCount<=15){
+      setTempColor("cold")
+    }
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+           <div>
+            <div>
+              <div className='box'>
+                {tempCount} C
+              </div>
+            </div>
+            <div>
+              <button onClick={()=> setTempCount(tempCount-1)}>-</button>
+              <button onClick={()=> setTempCount(tempCount+1)}>+</button>
+            </div>
+           </div>
     </div>
   )
 }
