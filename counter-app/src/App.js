0030872ed39615b4fa4f739060b53f4dbc82df8f
Roshan Arikar <85448130+roshanarikar@users.noreply.github.com>
2023-01-07 20:47:08 +0530

    counter app done

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div style={{"border":"1px solid black","width":"50%","margin":"auto","background":"grey","paddingBottom":"20px"}}>
+        <h1>Counter App</h1>       
+        <h1>{count}</h1>
+        <button style={{"width":"50px","height":"30px","fontSize":"10px"}} onClick={()=> setCount(0) }>Reset</button>
+        <div>
+        <button style={{"width":"50px","height":"50px","fontSize":"30px"}} onClick={()=> setCount(count-1)}>-</button>
+        <button style={{"width":"50px","height":"50px","fontSize":"30px"}} onClick={()=> setCount(count+1)}>+</button>
+        </div>
+       </div>
     </div>
   );
 }
